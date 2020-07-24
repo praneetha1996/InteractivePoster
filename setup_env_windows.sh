7de1eaceb7cb8049fa543114671b606ba7b7cd3a
Rafael Zamora-Resendiz <15003285+rz4@users.noreply.github.com>
2020-07-24 16:33:15 -0400

    Create setup_env_windows.sh

---
+++ setup_env_windows.sh
@@ -0,0 +1,16 @@
+
+# Make Virtual Environment
+virtualenv iposter-env
+source iposter-env/bin/activate
+
+# Install Dependencies
+pip install dash
+pip install plotly
+pip install pandas
+pip install dash-bootstrap-components
+pip install pypng
+pip install pyqrcode
+pip install gunicorn
+
+# Deactivate VE
+deactivate
